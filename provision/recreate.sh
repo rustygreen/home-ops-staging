55445f069e67731a74c41a6bab648b02029bc676
Rusty Green <rusty@rustygreen.com>
2022-08-10 10:28:15 -0400

    chore: updates

--- provision/recreate.sh
+++ provision/recreate.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-task ansible:nuke
-task ansible:list
-task ansible:prepare
-task ansible:reboot
-task ansible:list
-task ansible:install
-task cluster:nodes
-task cluster:verify
+task ansible:nuke &&
+task ansible:list &&
+task ansible:init &&
+task ansible:prepare &&
+task ansible:reboot &&
+task ansible:list &&
+task ansible:install &&
+task cluster:nodes &&
+task cluster:verify &&
 task cluster:install
